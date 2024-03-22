4462713f57cb748298b98f3dcfc4b82f3303b384
Stephen Kim <32920957+stephenk98@users.noreply.github.com>
2024-03-22 15:52:50 -0400

    Added error handling to the geocode_location function

--- weather.py
+++ weather.py
@@ -21,14 +21,17 @@ def get_reverse_geocode(lat, lon):
 def get_location_geocode(city, state, country):
     query_string = ','.join([x for x in [city, state, country] if x and x != ''])
     resp = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={query_string}&appid={open_weather_api_key}")
-    resp_data = resp.json()[0]
-    return {
-        'lat': resp_data.get('lat'),
-        'lon': resp_data.get('lon'),
-        'name': resp_data.get('name'),
-        'state': resp_data.get('state'),
-        'country': resp_data.get('country')
-    }
+    if len(resp.json()) == 0:
+        return None
+    else:
+        resp_data = resp.json()[0]
+        return {
+            'lat': resp_data.get('lat'),
+            'lon': resp_data.get('lon'),
+            'name': resp_data.get('name'),
+            'state': resp_data.get('state'),
+            'country': resp_data.get('country')
+        }
 
 def get_current_weather(lat, lon):
     resp = requests.get(f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={open_weather_api_key}&exclude=minutely")
